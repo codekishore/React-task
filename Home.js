07d1ad6e4e0d6dd88bfdf2e3292f93c1e1ccd664
Kishore <93311587+codekishore@users.noreply.github.com>
2022-10-29 12:03:18 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "../Components/Layouts/Navbar";
+import "../Pages/css Files/home.css";
+
+function Home() {
+  return (
+    <div className="home">
+     <h1>Welcome to Home Page!</h1>
+    </div>
+  );
+}
+
+<div className="container">
+  <div className="img">
+     
+  </div>
+
+</div>
+
+
+
+
+export default Home;
